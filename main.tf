15168ec718bf333564ada3f730faf51bfa9affa4
JustusK <Justus.Klauser@gmail.com>
2025-02-15 11:49:26 +0000

    Changed ReadME

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "my-first-project-451009"
+  region = "us-west1"
+}
+
+resource "google_storage_bucket" "mybucket" {
+  name = "my-terraform-bucket-12345"
+  location = "us-west1"
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.mybucket.name
+}
