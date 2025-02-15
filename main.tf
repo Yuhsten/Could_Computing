9de8c823dd58165793eaf242ca966d5072002759
JustusK <Justus.Klauser@gmail.com>
2025-02-15 14:02:08 +0000

    Reworked Code

--- main.tf
+++ main.tf
@@ -1,33 +1,37 @@
-terraform {
-  required_providers {
-    google = {
-        source = "hashicorp/google"
-        version = "~> 5.0"
-    }
-  }
-}
+# Declarative approach: Let the earth be like this. 
 
-resource "google_storage_buckets" "example"{
-    name = "my-terraform-bucket-12345"
-    region = "us-west1"
-    
-    versioning {
-        enabled = true
-    }
-
-    lables = {
-        enviroment = "student-lab"
-        managed_by = "terraform"
-    }
-}
-
+# We need a certain provider, which is google
+terraform {}
 
 provider "google" {
-  project = "my-first-project-451009"
-  region = "us-west1"
+    project = "warm-rookery-449616-f8" # Own project ID
+    region = "us-central1-a"
 }
 
+# We give the bucket a name and create Google Cloud Storage bucket
+resource "google_compute_instance" "vm_instance" {
+    name = "student-vm" # Ensure unique ID
+    machine_type = "e2-medium"
+    zone = "europe-west1-b"
+  
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  
+  network_interface {
+    
+    network = "default"
+    access_config {} # Enables external access optional
+      
+    }
+  }
+  
+# Define a output variable bucket_name
+output "vm_external_ip" {
+    value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+    }
 
-output "bucket_name" {
-  value = google_storage_bucket.mybucket.name
-}
+
